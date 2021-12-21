5c35491686755d347f2cd852b9a154f0c2419780
Raphael Ickovics <raphealickovics@gmail.com>
2021-12-21 16:25:23 +0000

    made behviour persistent, until now each flash switched the initial setting on one of the pins

--- blink-leds/src/main.c
+++ blink-leds/src/main.c
@@ -1,14 +1,14 @@
 #include <msp430.h>
 
-
 int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-	//PMSCTL0 &= ~LOCKLPMS;
 
 	P1DIR |= 0x01; // P1.0 output
 	P1DIR |= 0x01 << 6; // P1.6 output
-	P1OUT ^= 0x01;
+
+	P1OUT &= ~0x01;  // P1.0 off
+	P1OUT |= 0x01 << 6;  // P0.6 on
 
 	for (;;)
 	{
